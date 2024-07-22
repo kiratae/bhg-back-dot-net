cfe85e16621b48a193e399f7ccf9c245219cb4d9
tanap <tanap@KIRATAE-PC>
2024-07-22 17:48:15 +0700

    Add project files.

---
+++ WebService/Hubs/RoomHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace BHG.WebService
+{
+    public class RoomHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
